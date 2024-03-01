65b10d3a52c082bfedc8db1fa69c497eca1d6b20
jmespino00 <jbespino@up.edu.ph>
2024-03-01 22:30:55 +0800

    Chore: Updated some functions

---
+++ exer3/README.md
@@ -0,0 +1,5 @@
+Full Name: ESPINO, Jannah Marssell B. 
+Degree Program: Computer Science 
+Live Website Link: jmespino.github.io 
+Key takeaways from the exercise? For me, javascript is kind of like the other languages in terms 
+of how the data types and functions work. They are familiar.
--- exer3/exer3.js
+++ exer3/exer3.js
@@ -1,91 +1,81 @@
 console.log("Password:\n");
 
-var pw1;
-var pw2;
+var pw1 = "";
+var pw2 = "";
 
 
-function minChar(){
- if (pw1.length > 7 && pw2.length >7) {
+function minChar(pw1, pw2){//checks pw character
+   if (pw1.length > 7 && pw2.length > 7){
     return true;
- }
+   } else {
+    return false;
+   }
 }
 
-function cNum(){
-    let a = false;
-    let b = false;
-    var i=0;
-    var character='';
-    while (i <= pw1.length){
-        character = pw1.charAt(i);
-        if (!isNaN(character * 1)){
-            a = true;
+function cNum(pw1, pw2) {//checks if it has numbers
+    for (let i = 0; i < pw1.length; i++) {
+        var character = pw1.charAt(i);
+        if (!isNaN(character * 1)) {
+            return true;
         }
     }
 
-    while (i <= pw2.length){
-        character = pw2.charAt(i);
-        if (!isNaN(character * 1)){
-            b = true;
+    for (let i = 0; i < pw2.length; i++) {
+        var character = pw2.charAt(i);
+        if (!isNaN(character * 1)) {
+            return true;
         }
     }
-    i++;
+    return false;
 }
     
 
-function cUpperCase(){
+function cUpperCase(pw1, pw2){//checks if it has uppercase
     if (pw1 == pw1.toUpperCase() && pw2 == pw2.toUpperCase()){
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 
-function cLowerCase(){
+function cLowerCase(pw1, pw2){//checks if it has lowercase
     if (pw1 == pw1.toLowerCase() && pw2 == pw2.toLowerCase()){
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 
 
 
-function validatePassword(pw1, pw2){
-if (pw1 === pw2 && minChar == true && cLowerCase == true && cUpperCase == true){
-    return true;
-}else{
+function validatePassword(pw1, pw2){//validates all the prev functions
+if (pw1 !== pw2) {
     return false;
 }
+return minChar(pw1, pw2) && cLowerCase(pw1, pw2) && cUpperCase(pw1, pw2) && cNum(pw1, pw2);
 }
 
-function reversePassword(pw1, pw2){
+function reversePassword(pw){//reverses the new pw
     let reversed = '';
-    for (let i = pw1.length - 1; i >= 0; i--) {
-        reversed += pw1[i];
+    for (let i = pw.length - 1; i >= 0; i--) {
+        reversed += pw[i];
     }
     return reversed;
 }
 
-function storePassword(name, pw1, pw2){
+function storePassword(name, pw1, pw2){//stores the password and prints it
     var newPW = pw2;
-    if (validatePassword(pw1, pw2) == true){
+    if (validatePassword(pw1, pw2)){
         newPW = reversePassword(pw1, pw2);
-    }else{
-        newPW == pw2;
+    } else {
+        newPW = pw2;
     }
     let namePW = {
         name: name,
-        newpassword: newPW,
+        newpassword: reversePassword(pw1),
     }
-    console.log("name:" +name+ ", newpassword:" +newPW);
+    return namePW;
 }
-
-
-
-
-
-
-
 
 
 
@@ -100,3 +90,4 @@ console.log(validatePassword("HELLO1234", "HELLO1234"));  // returns false
 
 console.log(storePassword("John", "Pass1234", "Pass1234")); // returns {name: "John", newpassword:"4321ssaP"}
 console.log(storePassword("John", "Pass123", "Pass12345")); // returns {name: "John", newpassword:"Pass123"}
+
